14e4aa919c1d86b728ba57b5021f39402e42290c
MaryNayram <m.doh@alustudent.com>
2023-10-15 18:44:51 +0000

    CONSOLE

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A Fabric script"""
+
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """
+    generates .tgz archive from contents of web_static folder
+    """
+    local("mkdir -p versions")
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(current_time)
+    result = local("tar -czf {} web_static/".format(path))
+    if result.failed:
+        return None
+    return (path)
